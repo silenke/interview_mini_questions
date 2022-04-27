d153915a55dc95161796913bd6c3b41636cec0ca
silenke <2595756713@qq.com>
2022-04-27 17:22:06 +0800

    添加线程安全的队列。

---
+++ thread_safe_queue_1/源.cpp
@@ -0,0 +1,81 @@
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <cassert>
+#include <optional>
+
+template <class T>
+class Queue {	// 无界队列
+public:
+	//Queue(const Queue&) = delete;
+	//Queue& operator=(Queue&) = delete;
+
+	void push(const T& val) {
+		emplace(val);
+	}
+
+	void push(T&& val) {
+		emplace(std::move(val));
+	}
+
+	template <class... Args>
+	void emplace(Args&&... args) {
+
+		std::lock_guard lk{ m_ };
+		q_.emplace(std::forward<Args>(args)...);
+		cv_.notify_one();
+	}
+
+	T pop() {
+
+		std::unique_lock lk{ m_ };
+		cv_.wait(lk, [this] { return !q_.empty(); });
+
+		assert(!q_.empty());
+
+		T ret{ std::move_if_noexcept(q_.front()) };
+		q_.pop();
+		return ret;
+	}
+
+	std::optional<T> try_pop() {
+
+		std::lock_guard lk{ m_ };
+		if (q_.empty()) return {};
+
+		std::optional<T> ret{ std::move_if_noexcept(q_.front()) };
+		q_.pop();
+		return ret;
+	}
+
+private:
+	std::queue<T> q_;
+	std::mutex m_;
+	std::condition_variable cv_;
+};
+
+#include <thread>
+#include <iostream>
+int main() {
+
+	Queue<int> q;
+	std::thread t1(
+		[&] {
+			for (int i = 0; i < 100; i++) {
+				q.push(i);
+			}
+		}
+	);
+	std::thread t2(
+		[&] {
+			for (int i = 0; i < 100; i++) {
+				//std::cout << q.pop() << " ";
+				if (auto ret = q.try_pop()) {
+					std::cout << *ret << " ";
+				}
+			}
+		}
+	);
+	t1.join();
+	t2.join();
+}
---
+++ thread_safe_queue_2/源.cpp
@@ -0,0 +1,92 @@
+#include <boost/circular_buffer.hpp>
+#include <mutex>
+#include <condition_variable>
+#include <cassert>
+#include <optional>
+
+template <class T>
+class Queue {	// 有界队列
+public:
+	Queue(size_t capacity) : q_{capacity} {}
+
+	template <class T>
+	void push(T&& val) {	// 阻塞
+		
+		std::unique_lock lk{ m_ };
+		not_full_.wait(lk, [this] { return !q_.full(); });
+
+		assert(!q_.full());
+
+		q_.push_back(std::forward<T>(val));
+		not_empty_.notify_one();
+	}
+
+	T pop() {	// 阻塞
+
+		std::unique_lock lk{ m_ };
+		not_empty_.wait(lk, [this] { return !q_.empty(); });
+
+		assert(!q_.empty());
+
+		T ret{ std::move_if_noexcept(q_.front()) };
+		q_.pop_front();
+		not_full_.notify_one();
+		return ret;
+	}
+	
+	template <class T>
+	bool try_push(T&& val) {	// 非阻塞
+
+		std::lock_guard lk{ m_ };
+		if (q_.full()) return false;
+
+		q_.push_back(std::forward<T>(val));
+		not_empty_.notify_one();
+		return true;
+	}
+
+	std::optional<T> try_pop() {	// 非阻塞
+
+		std::lock_guard lk{ m_ };
+		if (q_.empty()) return {};
+		
+		std::optional<T> ret{ std::move_if_noexcept(q_.front()) };
+		q_.pop_front();
+		not_full_.notify_one();
+		return ret;
+	}
+
+private:
+	boost::circular_buffer<T> q_;
+	std::mutex m_;
+	std::condition_variable not_full_;
+	std::condition_variable not_empty_;
+};
+
+#include <thread>
+#include <iostream>
+int main() {
+
+	Queue<int> q{ 10 };
+	std::thread t1(
+		[&] {
+			for (int i = 0; i < 100; ) {
+				if (q.try_push(i)) {
+					i++;
+				}
+			}
+		}
+	);
+	std::thread t2(
+		[&] {
+			for (int i = 0; i < 100; ) {
+				if (auto ret = q.try_pop()) {
+					std::cout << *ret << " ";
+					i++;
+				}
+			}
+		}
+	);
+	t1.join();
+	t2.join();
+}
